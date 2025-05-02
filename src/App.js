011a4f1f7f0bbfd46bf16bec9ab113f0e0062aaa
Divya Prakash Mishra <divya.mishra@velocis.co.in>
2025-05-02 13:06:27 +0530

    routing name change

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/roiform" />} />
-      <Route path="/roiform" element={<TableComponent />} />
-      <Route path="/roiform/add" element={<FormComponent mode="add" />} />
-      <Route path="/roiform/edit/:id" element={<FormComponent mode="edit" />} />
-      <Route path="/roiform/view/:id" element={<FormComponent mode="view" />} />
+      <Route path="/" element={<Navigate to="/boiform" />} />
+      <Route path="/boiform" element={<TableComponent />} />
+      <Route path="/boiform/add" element={<FormComponent mode="add" />} />
+      <Route path="/boiform/edit/:id" element={<FormComponent mode="edit" />} />
+      <Route path="/boiform/view/:id" element={<FormComponent mode="view" />} />
     </Routes>
   );
 }
